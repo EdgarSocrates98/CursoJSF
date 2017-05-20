b1aa54a6b39b411ef9be7c41e774b9437a6d0221
EdgarSocrates98 <edgarsocrates98@gmail.com>
2017-05-20 15:44:05 -0300

    Inicio do Projeto.

---
+++ AulasJsfPrimeFaces/src/br/com/framework/impl/crud/VariavelConexaoUtil.java
@@ -0,0 +1,13 @@
+package br.com.framework.impl.crud;
+
+/**
+ * Nome do Caminjo do JNDI datasource Tomcat
+ * @author EDGAR
+ *
+ */
+
+public class VariavelConexaoUtil {
+	
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+	
+}
